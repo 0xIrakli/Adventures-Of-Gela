2ff426da699d00a4bfab2a3e3106a86634654494
Butu <butu.irakli@gmail.com>
2022-05-07 15:42:37 +0400

    add player movement

--- __main__.py
+++ __main__.py
@@ -2,6 +2,13 @@ from src import *
 from player import Player
 
 win = init((800, 800))
+MOVEMENT = {
+    pygame.K_a: [-1, +0],
+    pygame.K_d: [+1, +0],
+    pygame.K_s: [+0, +1],
+    pygame.K_w: [+0, -1]
+}
+
 
 base = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -31,16 +38,15 @@ def build_levels(levels):
                 img.paste(level[base[y][x]], box=(x*8, y*8, (x+1)*8, (y+1)*8))
     img.save('img.png')
 
-build_levels(levels)
-
 def main():
-    player = Player([20, 20])
-    
+    player = Player([10, 10])
+    clock = pygame.time.Clock()
     while True:
+        win.fill((51, 51, 51))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
                 
-        player.draw(win)
+        player.update(win, clock.tick(60))        
         disp.update()
 main()
--- player.py
+++ player.py
@@ -4,6 +4,7 @@ class Player:
     def __init__(self, pos):
         self.pos = pos
         self.v = [0, 0]
+        self.speed = 4
     
     @property    
     def x(self):return self.pos[0]
@@ -14,12 +15,23 @@ class Player:
     @property
     def vy(self):return self.v[1]
     
-    def update(self):
-        self.pos[0] += self.v[0]
-        self.pos[1] += self.v[1]
+    def update(self, win, deltaT):
+        self.look()
+        self.move(deltaT)
+        self.draw(win)
     
-    def move(self, dirc):
-        self.v = dirc
+    def look(self):
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_a]: self.v[0] = -self.speed
+        if keys[pygame.K_d]: self.v[0] =  self.speed
+        if keys[pygame.K_w]: self.v[1] = -self.speed
+        if keys[pygame.K_s]: self.v[1] =  self.speed
+    
+    def move(self, deltaT):
+        self.v[0] *= 0.8
+        self.v[1] *= 0.8
+        self.pos[0] += self.v[0]#*deltaT*0.001
+        self.pos[1] += self.v[1]#*deltaT*0.001
         
     def draw(self, win):
-        pygame.draw.ellipse(win, (255, 110, 110), (self.x-20, self.y-20, self.x+20, self.y+20), 2)
+        pygame.draw.ellipse(win, (255, 110, 110), (self.x, self.y, 40, 40), 2)
