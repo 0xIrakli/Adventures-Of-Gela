359eec7d1e8f4349e96599e7314e2a83359ab527
Butu <butu.irakli@gmail.com>
2022-05-08 00:29:12 +0400

    ASDASDasd

--- src/boss.py
+++ src/boss.py
@@ -4,14 +4,16 @@ import pygame
 
 class Boss(Entity):
     def __init__(self, pos):
+        self.alive = True
         super().__init__(pos)
         self.img = Image.open('assets/animations/boss.png')
         self.img = self.img.resize((self.img.width*4, self.img.height*4), Image.Resampling.NEAREST)
         self.img.save('AAA.png')
         self.imgs = pygame.image.load('AAA.png')
         
-    def update(self):
-        pass
+    def update(self, player):
+        if player.ready:
+            self.alive == False
 
     def draw(self, win):
         win.blit(self.imgs, (self.x-(self.img.width//2), self.y-(self.img.height//2)))
