337a32f8cda1b3e526de2a1f90d7dd63ac359eec
D-T-6 <tabatadzedima@protonmail.com>
2022-05-07 11:59:54 +0400

    commented the code

--- game-template.md
+++ game-template.md
@@ -3,8 +3,11 @@ Intro text goes here
 ---
 
 1. Question #1
-   - [possible answer]("next question index")
-   - [a different possible answer]("next question index")
+   <!-- 
+   - [possible answer text](the index of the next question)
+    -->
+   - [answer one](2)
+   - [answer two](7)
 
 2. Question #2
    - [answer one](3)
--- src/game.py
+++ src/game.py
@@ -4,35 +4,47 @@ from rich import print
 
 
 class Game:
+    # Current question key (the questions have keys instead of indicies)
     current_question = '1'
 
     def __init__(self, game_file_path: str):
         self.read_file(game_file_path)
 
+        # Print the intro text at initialization
         print(self.data['Intro'])
 
     def read_file(self, game_file_path: str):
         with open(game_file_path, 'r') as game_file:
             game_file_content: str = game_file.read()
 
+            # Create a parser object
             parser = Parser()
+            # Parse the game file content
             self.data = parser.parse_game_file_content(game_file_content)
 
     def loop(self) -> bool:
         while 1:
-            answers = self.data[self.current_question]['answers']
+            # Store the question object in a variable with a shorter name
+            cq = self.data[self.current_question]
 
-            possible_answers = [ans[0]
-                                for ans in answers]
+            # Store the answer tuples in a variable with a shorter name
+            answers = cq['answers']
 
-            print(self.data[self.current_question]['question'])
+            # Collect all possible answers in an array for easy handling
+            possible_answers = [ans[0] for ans in answers]
 
-            if len(self.data[self.current_question]['answers']) == 0:
+            # Print the question statement
+            print(cq['question'])
+
+            # If this question has no possible answers, end the loop
+            if len(cq['answers']) == 0:
                 break
             else:
+                # Otherwise, print all the possible answers
                 for i, answer in enumerate(possible_answers):
-                    print(f'{str(i + 1).zfill(2).replace("0"," ")})   {answer}')
+                    print(f' {str(i + 1).zfill(2).replace("0"," ")})   {answer}')
 
+            # Prompt user until a valid answer is given
             while 1:
                 ans = input("===> ")
 
@@ -42,5 +54,4 @@ class Game:
 
             ans = str(int(ans) - 1)
 
-            self.current_question = answers[possible_answers.index(
-                ans)][1]
+            self.current_question = answers[possible_answers.index(ans)][1]
