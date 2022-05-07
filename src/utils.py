96c3689f1c6174d69681bed6f9baad8c0a1ddb7f
Butu <butu.irakli@gmail.com>
2022-05-07 19:36:28 +0400

    Changed tilemap generation system

--- src/utils.py
+++ src/utils.py
@@ -3,13 +3,12 @@ from . import Image
 
 def generate_tilemap(res):
     tilemap = Image.open('assets/levels/0/tilemap.png')
-
     tiles = [
         [
-            tilemap.crop((x*res, y*res, (x*res)+res, (y*res)+res))
-            for x in range(tilemap.width // res)
-        ]
-        for y in range(tilemap.height // res)
+            [
+                tilemap.crop((x, y, x+res, y+res)),
+                tilemap.crop((x+res, y, x+res*2, y+res))
+            ] for x in range(0, tilemap.width, res*2)
+        ] for y in range(0, tilemap.height, res)
     ]
-
     return tiles
